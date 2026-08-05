6fa8ad16d3a153fbde34bcc711e4c20c5082f4c6
DALI <angepaterne.dali@gmail.com>
2026-08-06 01:21:39 +0200

    finish UI navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,34 +19,43 @@ const Navbar = () => {
           <header className={cn("fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b-2")}>
                <nav className="flex items-center justify-between px-4 py-2 h-12">
                     {/* Logo */}
-                    <button>ZD</button>
+                    <button className="font-extrabold cursor-pointer uppercase font-logo">
+                         <span className="text-primary-gradient">ZD</span>
+                         <span>.</span>
+                    </button>
+
                     {/* Desktop nav links */}
-                    <div>
+                    <div className="hidden md:flex items-center gap-2">
                          {navLinks.map((link) => (
-                              <button key={link.navKey} className={cn("px-4 py-2 text-sm font-medium")}>
+                              <button
+                                   key={link.navKey}
+                                   className="px-4 py-2 text-sm font-medium  transition-discrete duration-200 hover:text-primary-gradient hover:cursor-pointer"
+                              >
                                    {link.label}
                               </button>
                          ))}
                     </div>
 
                     {/* Desktop right actions */}
-                    <Button size="icon" variant="black">
-                         <Moon size={16} />
-                    </Button>
-                    <Button variant="black">
-                         <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="currentColor"
-                              viewBox="0 0 16 16"
-                         >
-                              {/* pris depuis https://icons.getbootstrap.com/ */}
-                              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                         </svg>
-                         GitHub
-                    </Button>
-                    <Button size="md">CV↓</Button>
+                    <div className="hidden md:flex items-center gap-2">
+                         <Button size="icon" variant="black">
+                              <Moon size={16} />
+                         </Button>
+                         <Button variant="black">
+                              <svg
+                                   xmlns="http://www.w3.org/2000/svg"
+                                   width="16"
+                                   height="16"
+                                   fill="currentColor"
+                                   viewBox="0 0 16 16"
+                              >
+                                   {/* pris depuis https://icons.getbootstrap.com/ */}
+                                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                              </svg>
+                              GitHub
+                         </Button>
+                         <Button size="md">CV↓</Button>
+                    </div>
                </nav>
           </header>
      );
